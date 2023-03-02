cf917054c6f22c48734f8eff87234ebbb76377a7
jloveric <john.loverich@gmail.com>
2023-03-01 19:33:58 -0800

    Basic tests

--- deep_nearest_neighbor/layer.py
+++ deep_nearest_neighbor/layer.py
@@ -1,25 +1,36 @@
 import torch
 
 
-def layer(keys: torch.Tensor, values: torch.Tensor):
-    
+def layer(keys: torch.Tensor, values: torch.Tensor) -> torch.Tensor:
     distances = []
-    for value in values :
-        delta = keys-value
-        dist = torch.linalg.norm(delta,dim=1)
+    for value in values:
+        delta = keys - value
+        dist = torch.linalg.norm(delta, dim=1)
         distances.append(dist)
-
 
     res = torch.stack(distances)
     return res
 
 
-def new_keys(distances, target_classification, sample_classification ) :
-    nearest_neighbor = torch.argmin(distances,dim=1)
+def incorrect_predictions(
+    distances, target_classification, sample_classification
+) -> torch.Tensor:
+    """
+    Compute the sample classifications that did not match the predicted
+    classifications.  Return the indices that were computed incorrectly
+    :param distance: Distances from each of the neighbors
+    :param target_classification: The classification for each of those neighbors
+    :param sample_classification: The classification for each of the samples
+    """
+    nearest_neighbor = torch.argmin(distances, dim=1)
     predicted_classification = target_classification[nearest_neighbor]
 
-    print('predictions', predicted_classification)
-    res = (predicted_classification==sample_classification)
+    all = predicted_classification == sample_classification
+    wrong_indices = torch.logical_not(all).nonzero().squeeze()
 
-    
-    print('res', res)
+    return wrong_indices
+
+
+def extend_keys(keys: torch.Tensor, new_keys: torch.Tensor) -> torch.Tensor:
+    keys = torch.cat([keys, new_keys],dim=0)
+    return keys
--- tests/test.py
+++ tests/test.py
@@ -1,22 +1,33 @@
-from deep_nearest_neighbor.layer import layer, new_keys
+from deep_nearest_neighbor.layer import layer, incorrect_predictions, extend_keys
 import torch
 
 
 def test_layer():
     keys = torch.rand(5, 10)
-    key_classification = torch.randint(low=0, high=5, size=(keys.shape[0],))
+    print('keys.shape', keys.shape)
+
+    key_classification = torch.randint(low=0, high=2, size=(keys.shape[0],))
 
     values = torch.rand(20, 10)
-    value_classification = torch.randint(low=0, high=5, size=(values.shape[0],))
+    value_classification = torch.randint(low=0, high=2, size=(values.shape[0],))
+
 
     ans = layer(keys=keys, values=values)
-    print("ans", ans)
 
-    final = new_keys(
+    wrong_indices = incorrect_predictions(
         distances=ans,
         target_classification=key_classification,
         sample_classification=value_classification,
     )
-    print("final", final)
+    
+    
+    new_keys = values[wrong_indices]
+    print('new_keys.shape', new_keys.shape)
 
-    return ans
+    keys = extend_keys(keys, new_keys)
+
+    print("keys.shape", keys.shape)
+    assert keys.shape[1] == 10
+    assert keys.shape[0] >= 5
+
+
