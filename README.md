bc432bad5b80fdf79efbaa20b16dc0f3268c63e6
John <john.loverich@gmail.com>
2023-03-11 08:12:23 -0800

    Update readme, start work on language generation example

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # Dynamic and Deep Nearest Neighbors in Pytorch
 This algorithm does nearest neighbor approximation, building the
-approximation in batch mode. Each batch adds "centers" or "neighbors" that are not approximated correctly, so instead of using the entire dataset as the list of neighbors, only uses a subset. The entire thing runs on the GPU using pytorch.  During evaluation, instead of taking the k-nearest neighbor a weighted average of all neighbors
-is used to comput the output. This is very similar to fully connected
-layers in neural networks, but it only every requires one epoch. The plan is to extend this to multiple layers.
+approximation in batch mode.  This could also be thought of as a radial basis function network built dynamically. Each batch adds the "centers" or "neighbors" that are not approximated correctly, so instead of using the entire dataset as the list of neighbors, only uses a subset. The entire thing runs on the GPU using pytorch.  During evaluation, instead of taking the k-nearest neighbor a weighted average of all neighbors
+is used to compute the output (the weighting is a function of the inverse distance).
 
 # Invariant MNIST
 To run
@@ -23,8 +22,12 @@ test_result Results(error=0.0351, accuracy=0.9649, incorrect=351, total=10000)
 neighbors in model 3255
 ```
 
-# Image compression using nearest neighbor approximation
-Run
+# Image approximation using weighted neighbor approximation
+The example below is approximately 1/10 data compression of the original image.
+The left image shows the locations of the "centers", the right image shows the
+approximation of the original using these centers. Training completes in a 3
+seconds with large batch=256 on a NVidia 2080 and uses about 16,000 centers of the
+196,608 centers available in the data set.
 ```
 python examples/image_interpolation.py tolerance=0.2 target_accuracy=0.8
 ```
---
+++ examples/language_generation.py
@@ -0,0 +1,136 @@
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+from omegaconf import DictConfig, OmegaConf
+import hydra
+from deep_nearest_neighbor.layer import Layer, euclidian_distance, cosine_distance
+from deep_nearest_neighbor.networks import Network
+import os
+from pathlib import Path
+
+
+class TransformFlat:
+    def __init__(self):
+        self.to_tensor = ToTensor()
+
+    def __call__(self, input_data):
+        data = self.to_tensor(input_data).flatten()
+        return data
+
+
+training_data = datasets.MNIST(
+    root="data", train=True, download=True, transform=TransformFlat()
+)
+
+test_data = datasets.MNIST(
+    root="data", train=False, download=True, transform=TransformFlat()
+)
+
+
+def run_single_layer(cfg: DictConfig):
+    train_dataloader = DataLoader(
+        training_data,
+        batch_size=cfg.batch_size,
+        shuffle=True,
+        pin_memory=cfg.pin_memory,
+    )
+    test_dataloader = DataLoader(
+        test_data, batch_size=cfg.batch_size, shuffle=True, pin_memory=cfg.pin_memory
+    )
+    # print(f"hydra.run.dir", hydra.run.dir)
+    print(f"Current working directory : {os.getcwd()}")
+    # print(f"Orig working directory    : {get_original_cwd()}")
+    layer = Layer(
+        num_classes=10,
+        distance_metric=euclidian_distance,
+        device=cfg.device,
+        target_accuracy=cfg.target_accuracy,
+        max_neighbors=cfg.max_neighbors,
+    )
+
+    layer.epoch_loop(
+        dataloader=train_dataloader,
+    )
+    num_neighbors = len(layer.neighbors)
+
+    print("neighbors.device", layer.neighbors.device)
+    print("neighbor_class.device", layer.neighbor_class.device)
+
+    directory = Path(os.getcwd())
+    torch.save(layer.neighbors, str(directory / "neighbors.pt"))
+    torch.save(layer.neighbor_class, str(directory / "neighbor_classes.pt"))
+
+    train_result = layer.test_loop(
+        dataloader=train_dataloader,
+    )
+    print("train_result", train_result)
+
+    test_result = layer.test_loop(
+        dataloader=test_dataloader,
+    )
+
+    print("test_result", test_result)
+    print("neighbors in model", num_neighbors)
+
+
+def run_network(cfg: DictConfig):
+    train_dataloader = DataLoader(
+        training_data,
+        batch_size=cfg.batch_size,
+        shuffle=True,
+        pin_memory=cfg.pin_memory,
+    )
+    test_dataloader = DataLoader(
+        test_data, batch_size=cfg.batch_size, shuffle=True, pin_memory=cfg.pin_memory
+    )
+    # print(f"hydra.run.dir", hydra.run.dir)
+    print(f"Current working directory : {os.getcwd()}")
+    # print(f"Orig working directory    : {get_original_cwd()}")
+    network = Network(
+        dataloader=train_dataloader,
+        num_classes=10,
+        distance_metric=euclidian_distance,
+        device=cfg.device,
+        target_accuracy=cfg.target_accuracy,
+        max_neighbors=cfg.max_neighbors,
+    )
+
+    network.train()
+    """
+    layer.epoch_loop(
+        dataloader=train_dataloader,
+    )
+    num_neighbors = len(layer.neighbors)
+
+    print("neighbors.device", layer.neighbors.device)
+    print("neighbor_class.device", layer.neighbor_class.device)
+
+    directory = Path(os.getcwd())
+    torch.save(layer.neighbors, str(directory / "neighbors.pt"))
+    torch.save(layer.neighbor_class, str(directory / "neighbor_classes.pt"))
+
+    train_result = layer.test_loop(
+        dataloader=train_dataloader,
+    )
+    print("train_result", train_result)
+
+    test_result = layer.test_loop(
+        dataloader=test_dataloader,
+    )
+
+    print("test_result", test_result)
+    print("neighbors in model", num_neighbors)
+    """
+
+
+@hydra.main(config_path="../config", config_name="mnist", version_base="1.3")
+def run(cfg: DictConfig):
+    run_single_layer(cfg=cfg)
+    # run_network(cfg=cfg)
+
+
+if __name__ == "__main__":
+    run()
