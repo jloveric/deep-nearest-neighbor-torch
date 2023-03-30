94bd2059f83a44be3e0faa02a4c8fee4bb683534
jloveric <john.loverich@gmail.com>
2023-03-29 19:23:03 -0700

    Working on a network for a learned transform

---
+++ deep_nearest_neighbor/learned_transform.py
@@ -0,0 +1,47 @@
+from pytorch_lightning import LightningModule
+from deep_nearest_neighbor.metrics import CosineDistance
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+
+class DeepNearestNeighborLayer(LightningModule):
+    def __init__(self, in_features: int, out_features: int):
+        super().__init__()
+        self.l1 = nn.Linear(in_features, out_features)
+        self._centers = torch.tensor([])
+        self._values = torch.tensor([])
+        self._transformed_centers = torch.tensor([])
+        self._transformed_values = torch.tensor([])
+        self._distance_metric = CosineDistance(epsilon=1e-2, exponent=-2)
+
+    def forward(self, x):
+        return torch.relu(self.l1(x.view(x.size(0), -1)))
+
+    def add_keys(self, x):
+        keys = self.l1(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+
+        size = x.shape[0]
+        xa = x[:size, ...]
+        xb = x[size:, ...]
+        ya = y[:size, ...]
+        yb = y[size:, ...]
+
+        txa = self.l1(xa)
+        txb = self.l1(xb)
+
+        # calculate the nearest neighbors a->b and b->a. We
+        # reverse the role of keys and values to get the most
+        # out of the data.
+        ans_a = self._distance_metric(keys=txa, values=txb)
+        ans_b = self._distance_metric(keys=txb, values=txa)
+
+        loss = F.cross_entropy(ans_a, ya) + F.cross_entropy(ans_b, yb)
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.02)
